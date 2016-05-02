30058a8b0489a7e9a66d4c8dd74e328b3c7c3e38
cathal moore <cathal moore>
2016-05-02 09:46:06 +0100

    last uplaod included sql file for database

--- projectFirstAttempt/database/martialarts.sql
+++ projectFirstAttempt/database/martialarts.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Apr 15, 2016 at 07:16 AM
+-- Generation Time: May 02, 2016 at 01:43 AM
 -- Server version: 10.1.9-MariaDB-log
 -- PHP Version: 5.6.16
 
@@ -23,21 +23,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attendence`
+-- Table structure for table `attendences`
 --
 
-CREATE TABLE `attendence` (
+CREATE TABLE `attendences` (
   `id` int(11) NOT NULL,
-  `Date` date NOT NULL,
-  `studentId` int(11) NOT NULL
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `username` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `attendence`
+-- Dumping data for table `attendences`
 --
 
-INSERT INTO `attendence` (`id`, `Date`, `studentId`) VALUES
-(1, '2016-04-06', 1);
+INSERT INTO `attendences` (`id`, `date`, `username`) VALUES
+(19, '2016-04-28 20:50:38', 'cathal'),
+(20, '2016-04-28 20:51:31', 'cathal'),
+(34, '2016-04-28 21:21:05', 'bob'),
+(35, '2016-04-28 21:21:13', 'bob');
 
 -- --------------------------------------------------------
 
@@ -77,7 +80,7 @@ CREATE TABLE `students` (
   `username` varchar(20) NOT NULL,
   `lastGrade` char(1) NOT NULL,
   `currentGrade` char(1) NOT NULL,
-  `password` varchar(11) NOT NULL,
+  `password` varchar(60) NOT NULL,
   `dateJoined` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -86,29 +89,40 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `username`, `lastGrade`, `currentGrade`, `password`, `dateJoined`) VALUES
-(1, 'cathal', 'c', 'b', 'qwerty', '2015-12-02');
+(1, 'cathal', 'c', 'b', '$2y$10$HBJoofJh1YY8Tu/KZBDr7OZbQ.XVbayjIbhGjWwRoiAbeTcHqD.Bu', '2015-12-02'),
+(8, 'bob', 'h', 'd', '$2y$10$5XXGxPh9YDf/CcLF4gXxH.zCkDbLJ9RASipIlxFIj4kLtC8CSHBxO', '2015-12-31'),
+(9, 'admin', 'a', 'a', '$2y$10$3ylmBo9C.QTqSnqWDyzcT.BYC.amYposUsWCn.vBx9hyqQa8Yb71S', '2011-02-11');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `technique`
+-- Table structure for table `techniques`
 --
 
-CREATE TABLE `technique` (
+CREATE TABLE `techniques` (
   `id` int(11) NOT NULL,
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `techniques`
+--
+
+INSERT INTO `techniques` (`id`, `description`) VALUES
+(1, 'White belt to Blue Tag'),
+(2, 'White belt to Blue Tag'),
+(3, 'techniques \r\npadwork \r\nsparring \r\nkicks \r\nstretching'),
+(4, 'Grading Session');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `attendence`
+-- Indexes for table `attendences`
 --
-ALTER TABLE `attendence`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `studentId` (`studentId`);
+ALTER TABLE `attendences`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `grading`
@@ -133,9 +147,9 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `technique`
+-- Indexes for table `techniques`
 --
-ALTER TABLE `technique`
+ALTER TABLE `techniques`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -143,10 +157,10 @@ ALTER TABLE `technique`
 --
 
 --
--- AUTO_INCREMENT for table `attendence`
+-- AUTO_INCREMENT for table `attendences`
 --
-ALTER TABLE `attendence`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `attendences`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 --
 -- AUTO_INCREMENT for table `grading`
 --
@@ -161,41 +175,21 @@ ALTER TABLE `studentgrading`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
--- AUTO_INCREMENT for table `technique`
+-- AUTO_INCREMENT for table `techniques`
 --
-ALTER TABLE `technique`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `techniques`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `attendence`
---
-ALTER TABLE `attendence`
-  ADD CONSTRAINT `attendence_ibfk_1` FOREIGN KEY (`studentId`) REFERENCES `students` (`id`);
 
 --
 -- Constraints for table `grading`
 --
 ALTER TABLE `grading`
   ADD CONSTRAINT `grading_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
-
---
--- Constraints for table `studentgrading`
---
-ALTER TABLE `studentgrading`
-  ADD CONSTRAINT `studentgrading_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
-  ADD CONSTRAINT `studentgrading_ibfk_2` FOREIGN KEY (`grading id`) REFERENCES `grading` (`id`),
-  ADD CONSTRAINT `studentgrading_ibfk_3` FOREIGN KEY (`technique id`) REFERENCES `technique` (`id`);
-
---
--- Constraints for table `technique`
---
-ALTER TABLE `technique`
-  ADD CONSTRAINT `technique_ibfk_1` FOREIGN KEY (`id`) REFERENCES `studentgrading` (`technique id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
